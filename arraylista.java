5a040df67c164f6da2855e4ffeb6253aec665137
felipesabadini <fsabadini@hotmail.com>
2013-07-03 15:26:46 -0300

    Update arraylista.java

--- arraylista.java
+++ arraylista.java
@@ -1,2 +1,13 @@
 ArrayList<String> nome = new ArrayList<String>
-nome.add();
+nome.add("A");
+nome.add("B");
+nome.add("C");
+nome.add("D");
+
+System.out.println("letra: " + nome.get(0) );
+
+nome.remove(0);
+
+System.out.println("letra: " + nome.get(0) );
+
+nome.set(1,"WW");
